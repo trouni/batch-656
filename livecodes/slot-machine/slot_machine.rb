16f83cc66ea24c446d336ed65ddb45fd1e63542f
Trouni Tiet <trouni@gmail.com>
2021-07-14 18:44:53 +0900

    Add comments

--- livecodes/slot-machine/slot_machine.rb
+++ livecodes/slot-machine/slot_machine.rb
@@ -9,18 +9,20 @@ class SlotMachine
     60.times do
       shuffle_reels
       print @reels.join("\t") + "\r"
-      $stdout.flush
-      sleep(0.05)
+      $stdout.flush # this with '\r' resets the terminal output to create the animation
+      sleep(0.05) # we wait a little bit for the animation
     end
-    sleep(0.3)
+    sleep(0.3) # we wait a little bit for suspense
     @reels
   end
 
   def score
     if three_of_the_same?
+      # We can select any of the reels since they are identical
       unique_symbol = @reels.first
       symbol_value(unique_symbol) * 10
     elsif two_of_the_same_plus_joker?
+      # We sort the reels to make sure the middle reel is the doubled symbol
       double_symbol = @reels.sort[1]
       symbol_value(double_symbol) * 5
     else
@@ -41,10 +43,12 @@ class SlotMachine
   end
 
   def three_of_the_same?
+    # %w[🛎 🛎 🛎].uniq => ['🛎']
     @reels.uniq.length == 1
   end
 
   def two_of_the_same_plus_joker?
+    # %w[🛎 🤩 🛎].uniq => ['🛎', '🤩']
     @reels.uniq.length == 2 && @reels.include?('🤩')
   end
 end
